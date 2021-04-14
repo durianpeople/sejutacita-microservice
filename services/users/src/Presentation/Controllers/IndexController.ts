b120870671b304a439e9b8f67e08a01cbe25178c
Setya Wibawa <durianpeople@gmail.com>
2021-04-14 15:54:23 +0700

    controller & route update

--- services/users/src/Presentation/Controllers/IndexController.ts
+++ services/users/src/Presentation/Controllers/IndexController.ts
@@ -1,8 +1,15 @@
 import {NextFunction, Request, Response} from "express";
 import {Controller} from "expressjs-onion-provider";
+import {MessageBroker} from "../../MessageBroker";
+import {container} from "tsyringe";
 
 export class IndexController extends Controller {
+
     public index(request: Request, response: Response, next: NextFunction) {
-        response.send('Users 2')
+        let message_bus: MessageBroker = container.resolve(MessageBroker);
+        message_bus.broadcast('TestEvent', Buffer.from(JSON.stringify({
+            body: 'test'
+        })))
+        response.send('Users 3')
     }
 }
--- services/users/src/Presentation/routes.ts
+++ services/users/src/Presentation/routes.ts
@@ -1,6 +1,8 @@
+import 'reflect-metadata'
 import {Router} from "express";
 import {container} from "tsyringe";
 import {IndexController} from "./Controllers/IndexController";
 
 export const router: Router = Router()
-router.get('/', container.resolve(IndexController).index)
+let index_controller: IndexController = container.resolve(IndexController)
+router.get('/', index_controller.index)
